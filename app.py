85460f3358901adb90d9e2b9ee7b4bd67f0f42f1
Huzaifahdv <holmzsharlok66@gmail.com>
2024-10-16 15:57:45 +0600

    Modifying the database structure and adding other modifications

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from inputs import *
 
-
 if __name__ == "__main__":
-
     create_tables()
     while True:
+        print("\n" + "=" * 50)
         print("Please choose an operation:")
-        print("* To add a student, press 'a'")
-        print("* To delete a student, press 'd'")
-        print("* To update a student's information, press 'u'")
-        print("* To show a student's information, press 's'")
-        print("* To add a lesson, press 'al'")
-        print("* To delete a lesson, press 'dl'")
-        print("* To update a lesson, press 'ul'")
-        print("* To show a lessons, press 'sl'")
-        print("* To Exit the program, press 'q'")
-        choice = input("Enter your choice: ").lower()
+        print("=" * 50)
+        print("* To add a student, enter 'a'")
+        print("* To delete a student, enter 'd'")
+        print("* To update a student's information, enter 'u'")
+        print("* To show a student's information, enter 's'")
+        print("* To add a lesson, enter 'al'")
+        print("* To delete a lesson, enter 'dl'")
+        print("* To update a lesson, enter 'ul'")
+        print("* To show lessons information, enter 'sl'")
+        print("* To Exit the program, enter 'q'")
+        print("=" * 50)
+        choice = input("Enter your choice: ").lower().strip()
+
         if choice == 'a':
             add_student()
         elif choice == 'd':
@@ -33,6 +35,7 @@ if __name__ == "__main__":
         elif choice == 'sl':
             show_lesson()
         elif choice == 'q':
+            print("Exiting the program... Goodbye!")
             break
         else:
             print("Invalid choice. Please try again.")
